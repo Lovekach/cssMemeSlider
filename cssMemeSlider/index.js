0ce21d9d02719463c2454a199f35e3056d2b19bf
LoveKach <cryptonitik@gmail.com>
2023-09-12 16:25:31 +0300

    feat: add  some code on js  (Tue, Sep 12, 2023  4:25:31 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,32 +1,3 @@
-// slider === slider
-// slider_line === container-images
-// slider_wrapper === dots-group
-
-// const CONTAINER_IMG = document.querySelector('container-images'),
-// DOTS = document.querySelectorAll('dots'),
-// IMAGE = document.querySelectorAll('image');
-
-// let currentIndex = 0;
-
-// DOTS.addEventListener('click', function () {
-//     currentIndex = (currentIndex - 1 + DOTS.length) % DOTS.length;
-//     nextSlide();
-// });
-
-// function nextSlide() {
-//     CONTAINER_IMG.style.transform = `translateX(-${currentIndex * 934}px)`;
-
-//     DOTS.forEach((dot, index) => {
-//         if (index === currentIndex) {
-//             dot.classList.toggle('active');
-//         } else {
-//             dot.classList.remove('active');
-//         }
-//     });
-// }
-
-
-
 const IMAGE = document.querySelectorAll('.image'),
 DOTS = document.querySelectorAll('.dots'),
 IMAGE_WRAPPER = document.querySelector('.image-wrapper');
