0540d247477006ec76e85909311611358c08b754
LoveKach <cryptonitik@gmail.com>
2023-09-13 11:05:11 +0300

    fix: fix my code in  js file  (Wed, Sep 13, 2023 11:05:11 AM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -1,24 +1,38 @@
 const IMAGE = document.querySelectorAll('.image'),
 DOTS = document.querySelectorAll('.dots'),
-IMAGE_WRAPPER = document.querySelector('.image-wrapper');
+IMAGE_WRAPPER = document.querySelector('.image-wrapper'),
+STEP = document.querySelectorAll('.step'),
+STEPS_WRAPPER = document.querySelector('.steps-wrapper');
 
 let currentIndex = 0;
+let stepIndex = 0;
 
 function showImage(index) {
     IMAGE.forEach((image) => (image.style.opacity = 0));
     DOTS.forEach((dot) => dot.classList.remove('active'));
+    STEP.forEach((step) => step.classList.remove('active'));
 
     IMAGE[index].style.opacity = 1;
     DOTS[index].classList.add('active');
+    STEP[index].classList.add('active');
 
-    IMAGE_WRAPPER.style.transform = `translateX(-${index * 954}px)`;
+    setTimeout(() => {
+        STEP[index].classList.add('active');
+    }, 0);
+    IMAGE_WRAPPER.style.transform = `translateX(-${index * 904}px)`;
+   
 }
 
 DOTS.forEach((dot, index) => {
     dot.addEventListener('click', () => {
         currentIndex = index;
+        stepIndex = index;
         showImage(currentIndex);
+        showImage(stepIndex);
     });
 });
 
 showImage(currentIndex);
+showImage(stepIndex);
+
+
